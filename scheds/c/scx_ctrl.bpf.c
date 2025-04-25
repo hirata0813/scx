000b39a5f8a4f837f5954d2fcbc38ec3019654e7
hirata0813 <hirata2024@s.okayama-u.ac.jp>
2025-04-25 15:59:30 +0000

    Queuing can now be controlled by SCX program

--- scheds/c/scx_ctrl.bpf.c
+++ scheds/c/scx_ctrl.bpf.c
@@ -11,6 +11,8 @@ static u64 vtime_now;
 UEI_DEFINE(uei);
 
 #define SHARED_DSQ 0
+#define STOP 1
+#define RUN 0
 
 // PID LIST という MAP を定義
 // 1エントリに必要な情報：PID，フラグ(キューイングするかしないか)
@@ -23,15 +25,27 @@ struct {
 } pidlist SEC(".maps");
 
 // PIDを取得し，出力
-static s32 get_pid()
+static __u32 get_pid()
 {
-	return bpf_get_current_pid_tgid();
+	return (__u32)bpf_get_current_pid_tgid();
 }
+
+static int check_process_status(__u32 *pid)
+{
+	__u32 *flag;
+	flag = bpf_map_lookup_elem(&pidlist, pid);
+	if (flag && *flag == 1) //flagが1(つまり，MAPにPIDが存在し，そのflagが1)のとき
+	    return STOP;
+	else
+	    return RUN;
+}
+
 
 s32 BPF_STRUCT_OPS(simple_select_cpu, struct task_struct *p, s32 prev_cpu, u64 wake_flags)
 {
 	bool is_idle = false;
-	s32 cpu, pid; //s32 は，signed 32-bit integer
+	s32 cpu; //s32 は，signed 32-bit integer
+	__u32 pid = get_pid();
 
 	cpu = scx_bpf_select_cpu_dfl(p, prev_cpu, wake_flags, &is_idle); // 左の関数は，ops.select_cpu()のデフォルト実装
 	// 選択されたCPUがアイドル状態なら，id_idleにtrueが書き込まれる
@@ -39,9 +53,10 @@ s32 BPF_STRUCT_OPS(simple_select_cpu, struct task_struct *p, s32 prev_cpu, u64 w
 	// TODO: MAPで管理してないやつは普通にスケジューリングする
 	// TODO: MAPで管理してるやつは，flagをもとにスケジューリングする
 	if (is_idle) {
-		pid = get_pid();
-		bpf_printk("simple_select_cpu %d", pid);
-		scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL, SCX_SLICE_DFL, 0); // 選択されたCPUのローカルDSQにタスクを挿入
+		if (check_process_status(&pid) == RUN){
+			bpf_printk("simple_select_cpu %d", pid);
+			scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL, SCX_SLICE_DFL, 0); // 選択されたCPUのローカルDSQにタスクを挿入
+		}
 	}
 
 	return cpu;
@@ -58,18 +73,22 @@ void BPF_STRUCT_OPS(simple_enqueue, struct task_struct *p, u64 enq_flags)
 	if (fifo_sched) { //FIFOが有効化(つまり，ユーザ空間で-fオプションがついている)場合
 		scx_bpf_dsq_insert(p, SHARED_DSQ, SCX_SLICE_DFL, enq_flags); // グローバルDSQに挿入する
 	} else {
-		u64 vtime = p->scx.dsq_vtime;
+		if (check_process_status(&pid) == RUN){
 
-		/*
-		 * Limit the amount of budget that an idling task can accumulate
-		 * to one slice.
-		 */
-		if (time_before(vtime, vtime_now - SCX_SLICE_DFL)) //time_beforeは，第一引数の時刻が第二引数の時刻より前なら，trueを返す
-								   //common.bpf.h 内で定義されている
-			vtime = vtime_now - SCX_SLICE_DFL;
+			u64 vtime = p->scx.dsq_vtime;
 
-		scx_bpf_dsq_insert_vtime(p, SHARED_DSQ, SCX_SLICE_DFL, vtime,
-					 enq_flags);
+			/*
+			 * Limit the amount of budget that an idling task can accumulate
+			 * to one slice.
+			 */
+			if (time_before(vtime, vtime_now - SCX_SLICE_DFL)) //time_beforeは，第一引数の時刻が第二引数の時刻より前なら，trueを返す
+									   //common.bpf.h 内で定義されている
+				vtime = vtime_now - SCX_SLICE_DFL;
+
+			scx_bpf_dsq_insert_vtime(p, SHARED_DSQ, SCX_SLICE_DFL, vtime,
+						 enq_flags);
+
+		}
 	}
 }
 
