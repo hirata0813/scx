1dfe863b03dddbcd7c57bdb8e1ada5f81b61b1b7
hirata0813 <hirata2024@s.okayama-u.ac.jp>
2025-09-26 08:27:50 +0000

    enqueue()内で，CPU を均等に選び，固定するアルゴリズム(v3)

--- scheds/c/scx_priority.bpf.c
+++ scheds/c/scx_priority.bpf.c
@@ -50,8 +50,8 @@ struct {
     __uint(type, BPF_MAP_TYPE_HASH);
     __uint(max_entries, 8192);
     __type(key, pid_t);
-    __type(value, s32); /* flag: 1 if priority task */
-} prev_cpu_list SEC(".maps");
+    __type(value, s32); /* CPU assigned in enqueue() */
+} assigned_list SEC(".maps");
 
 /* Statistics */
 u64 nr_priority_local_sum = 0;
@@ -115,8 +115,6 @@ s32 BPF_STRUCT_OPS(priority_select_cpu, struct task_struct *p, s32 prev_cpu, u64
     //    }
     //}
     cpu = scx_bpf_select_cpu_dfl(p, prev_cpu, wake_flags, &dummy);
-    //cpu = bpf_get_prandom_u32() % 4;
-    //bpf_printk("in select_cpu: PID: %d, TGID: %d, CPU=%d\n", tgid, pid, cpu);
 
 
     //if (is_priority_task(p)) {
@@ -188,92 +186,67 @@ static s32 pick_cpu_based_on_cpumap()
 	return cpu_candidate;
 }
 
-
 void BPF_STRUCT_OPS(priority_enqueue, struct task_struct *p, u64 enq_flags)
 {
-    s32 current_cpu=scx_bpf_task_cpu(p);
-    s32 cpu;
-    s32 *val;
-    s32 *prev;
+    s32 cpu=scx_bpf_task_cpu(p);
+    s32 *assigned_cpu;
     u64 dummy;
+
     pid_t pid = p->pid;
     pid_t tgid = p->tgid;
 
     //bpf_printk("in enqueue: PID: %d, TGID: %d, CPU=%d\n", tgid, pid, cpu);
 
     if (is_priority_task(p)) {
-	s32 *tmp;
 
 	if (is_fixed_prior_task){
     		scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL_ON | priortask_cpu, SCX_SLICE_DFL, SCX_ENQ_HEAD);
 		return;
 	}
 
-	// cpu map について，以前用いていた CPU のタスク数を1減らす
-	prev = bpf_map_lookup_elem(&prev_cpu_list, &pid);
+	// assigned_list を確認し，enqueue()により CPU が割り当てられているか確認
+	assigned_cpu = bpf_map_lookup_elem(&assigned_list, &pid);
+	if (assigned_cpu == NULL) {
+		s32 *val;
 
-	// まず，prev_cpu_listから，以前のCPUを取得(エントリがない場合はエントリを作り，取得)
-	if (prev == NULL) {
-		bpf_map_update_elem(&prev_cpu_list, &pid, &current_cpu, BPF_ANY);	
-		prev = bpf_map_lookup_elem(&prev_cpu_list, &pid);
-    	}
+		cpu = pick_cpu_based_on_cpumap();
+		// 割り当てられていない場合，pick_cpu_based_on_cpumap により，タスクの CPU 選択 & assigned_list を更新
+		bpf_map_update_elem(&assigned_list, &pid, &cpu, BPF_ANY);	
 
-	// 次に，cpu_task_mapをprevで引いて，該当CPUのポインタをtmpに格納
-	if (prev != NULL){
-		tmp = bpf_map_lookup_elem(&cpu_task_map, prev);
-	}
+		val = bpf_map_lookup_elem(&cpu_task_map, &cpu);	
+		if (val != NULL){
+			__sync_fetch_and_add(val, 1);
+		}
 
-	if (tmp != NULL && *tmp > 0) {
-       		__sync_fetch_and_add(tmp, -1);
-    	}
+		// DSQ に追加
+        	scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL_ON | cpu, SCX_SLICE_DFL * priority_slice_multiplier, SCX_ENQ_HEAD);
+    	}else{
+		// 割り当てられている場合，その値を参照する
+        	scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL_ON | *assigned_cpu, SCX_SLICE_DFL * priority_slice_multiplier, SCX_ENQ_HEAD);
+	}	
 	
-	// CPU を選択
-        cpu = pick_cpu_based_on_cpumap();
-
-	// cpu_map を更新
-	val = bpf_map_lookup_elem(&cpu_task_map, &cpu);
-	if (val != NULL) {
-       		__sync_fetch_and_add(val, 1);
-    	}
-
-	// prev_cpu_list を更新
-	bpf_map_update_elem(&prev_cpu_list, &pid, &cpu, BPF_ANY);	
-
-        scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL_ON | cpu, SCX_SLICE_DFL * priority_slice_multiplier, SCX_ENQ_HEAD);
-
     } else if(is_nonpriority_task(p)){
-	s32 *tmp;
-	// cpu map について，以前用いていた CPU のタスク数を1減らす
-	prev = bpf_map_lookup_elem(&prev_cpu_list, &pid);
+	// assigned_list を確認し，enqueue()により CPU が割り当てられているか確認
+	assigned_cpu = bpf_map_lookup_elem(&assigned_list, &pid);
+	if (assigned_cpu == NULL) {
+		s32 *val;
 
-	// まず，prev_cpu_listから，以前のCPUを取得(エントリがない場合はエントリを作り，取得)
-	if (prev == NULL) {
-		bpf_map_update_elem(&prev_cpu_list, &pid, &current_cpu, BPF_ANY);	
-		prev = bpf_map_lookup_elem(&prev_cpu_list, &pid);
-    	}
+		cpu = pick_cpu_based_on_cpumap();
+		// 割り当てられていない場合，pick_cpu_based_on_cpumap により，タスクの CPU 選択 & assigned_list を更新
+		bpf_map_update_elem(&assigned_list, &pid, &cpu, BPF_ANY);	
 
-	// 次に，cpu_task_mapをprevで引いて，該当CPUのポインタをtmpに格納
-	if (prev != NULL){
-		tmp = bpf_map_lookup_elem(&cpu_task_map, prev);
-	}
+		val = bpf_map_lookup_elem(&cpu_task_map, &cpu);	
+		if (val != NULL){
+			__sync_fetch_and_add(val, 1);
+		}
 
-	if (tmp != NULL && *tmp > 0) {
-       		__sync_fetch_and_add(tmp, -1);
-    	}
+		// DSQ に追加
+        	scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL_ON | cpu, SCX_SLICE_DFL, 0);
+    	}else{
+		// 割り当てられている場合，その値を参照する
+        	scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL_ON | *assigned_cpu, SCX_SLICE_DFL, 0);
+	}	
 
-	
-	// CPU を選択
-        cpu = pick_cpu_based_on_cpumap();
-
-	// cpu_map を更新
-	val = bpf_map_lookup_elem(&cpu_task_map, &cpu);
-	if (val != NULL) {
-       		__sync_fetch_and_add(val, 1);
-    	}
-
-	// prev_cpu_list を更新
-	bpf_map_update_elem(&prev_cpu_list, &pid, &cpu, BPF_ANY);	
-        scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL_ON | cpu, SCX_SLICE_DFL * priority_slice_multiplier, 0);
     } else{
     	scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL, SCX_SLICE_DFL, 0);
     }
